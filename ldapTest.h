4cdba57993b64983d84e475c06d20d5b63707575
miang <miang@live.com>
2014-05-02 15:29:41 +0700

    server imported iosport

---
+++ ldapTest.h
@@ -0,0 +1,13 @@
+//
+//  ldapTest.h
+//  SocketKit
+//
+//  Created by miang on 5/2/2557 BE.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ldapTest : NSObject
+
+@end
---
+++ ldapTest.m
@@ -0,0 +1,13 @@
+//
+//  ldapTest.m
+//  SocketKit
+//
+//  Created by miang on 5/2/2557 BE.
+//
+//
+
+#import "ldapTest.h"
+
+@implementation ldapTest
+
+@end
